3ef765630402dcc346958bfd468cbed0ef975f08
Kei Tsuji <ktsujister@gmail.com>
2012-04-12 14:51:07 +0900

    update libraries.

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "create xls(or xlsx) file from tsv file."
   :dependencies [[org.clojure/clojure		"1.3.0"]
                  [org.clojure/tools.cli		"0.2.1"]
-                 [org.clojure/data.csv		"0.1.0"]
-                 [org.apache.poi/poi		"3.8-beta5"]
-                 [org.apache.poi/poi-ooxml	"3.8-beta5"]]
+                 [org.clojure/data.csv		"0.1.2"]
+                 [org.apache.poi/poi		"3.8"]
+                 [org.apache.poi/poi-ooxml	"3.8"]]
   :dev-dependencies [[swank-clojure		"1.4.0"]]
   :jvm-opts ["-Dfile.encoding=utf-8"]
   :main tsv2xls.core)

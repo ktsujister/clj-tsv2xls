70377b55bbab70774a08e6101bbf42ec2da08b75
Kei Tsuji <ktsujister@gmail.com>
2014-09-10 10:59:49 +0900

    lein-release plugin: bumped version from 0.0.4 to 0.0.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tsv2xls "0.0.4"
+(defproject tsv2xls "0.0.5-SNAPSHOT"
   :description "create xls(or xlsx) file from tsv file."
   :dependencies [[org.clojure/clojure		"1.6.0"]
                  [org.clojure/tools.cli		"0.3.1"]

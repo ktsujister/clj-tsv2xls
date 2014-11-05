14d198be2d83f55edafb755324337ad0ad610ac0
Kei Tsuji <ktsujister@gmail.com>
2014-11-05 15:12:34 +0900

    use simpler way of handling tsv files

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "create xls(or xlsx) file from tsv file."
   :dependencies [[org.clojure/clojure		"1.6.0"]
                  [org.clojure/tools.cli		"0.3.1"]
-                 [org.clojure/data.csv		"0.1.2"]
                  [org.apache.poi/poi		"3.9"]
                  [org.apache.poi/poi-ooxml	"3.9"]]
   :jvm-opts ["-Dfile.encoding=utf-8"]
